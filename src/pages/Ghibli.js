eb4b89764a6ea902e210bbcf51557868f21d5f6b
Robin <miguet.robin@gmail.com>
2023-05-26 16:49:27 +0200

    try to fix fetch pb. Not the final solution but just to display

--- src/pages/Ghibli.js
+++ src/pages/Ghibli.js
@@ -2,12 +2,12 @@ import React from "react";
 import Banner from "../components/Banner";
 import Gallery from "../components/Gallery";
 import Logo from "../components/Logo";
-// import ghibli from "./ghibli.png";
+import ghibli from "./ghibli.png";
 
 const Ghibli = () => {
   return (
     <div id="ghibli-page">
-      <Logo source="./assets/ghibli.png" />
+      <Logo source={ghibli} />
       <Banner />
       <Gallery />
     </div>
